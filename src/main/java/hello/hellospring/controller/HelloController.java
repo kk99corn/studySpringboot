5a7020d42344b7cc2e22fad4cf2ba492b5dd2986
99 <hahahoho5915@gmail.com>
2022-04-26 22:19:30 +0900

    feal: ResponseBody를 통한 데이터 전달

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,5 +13,50 @@ public class HelloController {
 	public String hello(Model model) {
 		model.addAttribute("data", "hello~~~");
 		return "hello";
+	}
+
+	// mvc
+	@GetMapping("hello-mvc")
+	public String helloMvc(@RequestParam(name = "name", required = false) String name, Model model) {
+		model.addAttribute("name", name);
+		return "hello-template";
+	}
+
+	@GetMapping("hello-string")
+	@ResponseBody
+	public String helloString(@RequestParam("name") String name) {
+		return "hello " + name;
+	}
+
+	// hello
+	@GetMapping("hello-api")
+	@ResponseBody
+	public Hello helloApi(@RequestParam("name") String name) {
+		Hello hello = new Hello();
+		hello.setName(name);
+		hello.setAge(200);
+		return hello;
+	}
+
+	static class Hello {
+		private String name;
+
+		public int getAge() {
+			return age;
+		}
+
+		public void setAge(int age) {
+			this.age = age;
+		}
+
+		private int age;
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
 	}
 }
